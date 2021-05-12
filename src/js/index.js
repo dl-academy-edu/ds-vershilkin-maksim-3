3d198285f8d0d137a0730932f4d11966ea918988
Max Vershilkin <vma90@inbox.ru>
2021-05-12 10:53:59 +0400

    Made the homework 3. Added the following files: developers.js and homework-3.js. Was edited index.js file.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import {numbers} from "./homework-1.js";
 import {functions} from "./homework-2.js";
+import {objects} from "./homework-3.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
   // Homework 1. Source: homework-1.js.
   // numbers(50);
   // Homework 2. Source: homework-2.js.
-  functions();
+  // functions();
+  // Homework 3. Source: homework-4.js.
+  objects();
 
 })
