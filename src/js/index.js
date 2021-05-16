ce1d9e655be2dfd5fae03b4aaee9c3889d8860be
Max Vershilkin <vma90@inbox.ru>
2021-05-16 13:50:34 +0400

    Was added the button for scrolling to the top of the page.

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 import {numbers} from "./homework-1.js";
 import {functions} from "./homework-2.js";
 import {objects} from "./homework-3.js";
+import {scrollTop as buttonUp} from "./homework-4.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  // numbers(50);
-  // objects();
+  buttonUp('button-up', '--visible', 1500);
 
 })
